ce4f2a74ece28a67a118ee8311214d2c51c10b05
kaveh norouzi <kaveh.norozi@live.com>
2022-01-05 17:42:20 +0330

    commit

--- PdfToImage/PdfToImage/Controllers/HomeController.cs
+++ PdfToImage/PdfToImage/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.Advanced;
+using PdfSharp.Pdf.IO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,21 +15,80 @@ namespace PdfToImage.Controllers
     {
         public ActionResult Index()
         {
+    
+
+
+            string FileSource = Server.MapPath(Url.Content("~/Uploads/nash.pdf"));
+            string files = Server.MapPath(Url.Content("~/Uploads/convert.png"));
+            PdfDocument document = PdfReader.Open(FileSource);
+            int imageCount = 0;
+            // Iterate pages
+            foreach (PdfPage page in document.Pages)
+            {
+                // Get resources dictionary
+                PdfDictionary resources = page.Elements.GetDictionary("/Resources");
+                if (resources != null)
+                {
+                    // Get external objects dictionary
+                    PdfDictionary xObjects = resources.Elements.GetDictionary("/XObject");
+                    if (xObjects != null)
+                    {
+                        var items = xObjects.Elements.Values;
+                        // Iterate references to external objects
+                        foreach (PdfItem item in items)
+                        {
+                            PdfReference reference = item as PdfReference;
+                            if (reference != null)
+                            {
+                                PdfDictionary xObject = reference.Value as PdfDictionary;
+                                // Is external object an image?
+                                //    if (xObject != null && xObject.Elements.GetString("/Subtype") == "/Image")
+                                if (xObject != null)
+                                {
+                                    ExportImage(xObject, ref imageCount);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             return View();
         }
-
-        public ActionResult About()
+        public void ExportImage(PdfDictionary image, ref int count)
         {
-            ViewBag.Message = "Your application description page.";
+            string filter = image.Elements.GetName("/Filter");
+            switch (filter)
+            {
+                case "/DCTDecode":
+                    ExportJpegImage(image, ref count);
+                    break;
 
-            return View();
+                case "/FlateDecode":
+                    ExportAsPngImage(image, ref count);
+                    break;
+            }
+
         }
-
-        public ActionResult Contact()
+        public void ExportJpegImage(PdfDictionary image, ref int count)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            // Fortunately JPEG has native support in PDF and exporting an image is just writing the stream to a file.
+            byte[] stream = image.Stream.Value;
+            string files = Server.MapPath(Url.Content(String.Format("~/Uploads/Image{0}.jpeg", count++)));
+            
+            FileStream fs = new FileStream(files, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(stream);
+            bw.Close();
         }
+        static void ExportAsPngImage(PdfDictionary image, ref int count)
+        {
+            int width = image.Elements.GetInteger(PdfImage.Keys.Width);
+            int height = image.Elements.GetInteger(PdfImage.Keys.Height);
+            int bitsPerComponent = image.Elements.GetInteger(PdfImage.Keys.BitsPerComponent);
+
+        
+        }
+
+
     }
 }
